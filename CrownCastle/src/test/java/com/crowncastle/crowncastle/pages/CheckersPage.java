aa9b55192a472f7d2dc1e2850639e927c492a8fb
Hogis23 <80396467+RazvanHLiveU@users.noreply.github.com>
2024-03-09 15:31:42 -0500

    Add Page Is Up Test for Checkers Game

--- CrownCastle/src/test/java/com/crowncastle/crowncastle/pages/CheckersPage.java
+++ CrownCastle/src/test/java/com/crowncastle/crowncastle/pages/CheckersPage.java
@@ -17,6 +17,11 @@ public class CheckersPage {
 
     public String page_url = "https://www.gamesforthebrain.com/game/checkers/";
 
+    @FindBy(xpath = "//*[text() = 'Checkers']")
+    public WebElement checkersTitle;
+
+    
+
     public CheckersPage(WebDriver driver) {
         this.driver = driver;
         driver.get(page_url);
@@ -26,8 +31,22 @@ public class CheckersPage {
 
 
 
-    public void navigateToPage(){
+    public boolean isPageLoaded(){
+        //waiting 10 seconds for the page to load
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String s = driver.getCurrentUrl();
+        // checking condition if the URL is loaded
+        return s.equals(page_url);
+    }
 
+    public boolean isTitleVisible() {
+
+        //the try catch is necessary because if the element is not on the page it will crash and the tests will not continue
+        try {
+            return  checkersTitle.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
 
     }
 
--- CrownCastle/src/test/java/com/crowncastle/crowncastle/tests/CheckersTests.java
+++ CrownCastle/src/test/java/com/crowncastle/crowncastle/tests/CheckersTests.java
@@ -2,6 +2,7 @@ package com.crowncastle.crowncastle.tests;
 
 import com.crowncastle.crowncastle.pages.CheckersPage;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,7 +22,16 @@ public class CheckersTests {
     @Test
     @Order(1)
     void checkPageIsUp(){
-        System.out.print("code from here");
+        //checking if the page url is correct
+       assert checkersPage.isPageLoaded();
+       System.out.println("Page loaded");
+       //to make sure that elements are visible we will also check for the "Checkers" title
+       assert checkersPage.isTitleVisible();
+        // Finally we will also check if the DOM of the page marks the page load as completed
+        JavascriptExecutor j = (JavascriptExecutor) driver;
+        System.out.println(j.executeScript("return document.readyState").toString());
+        assert j.executeScript("return document.readyState").toString().equals("complete");
+
     }
 
     @AfterAll()
